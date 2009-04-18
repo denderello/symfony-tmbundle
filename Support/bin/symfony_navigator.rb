ec2194a2a799d38670b85f5a63937bd0ac1c292e
ever.zet <ever.zet@gmail.com>
2009-04-18 09:38:25 +0300

    navigator preview. Press ctrl+f when on action signature in actions.class.php & you will enter in action template. Press ctrl+f in template & you will enter in actions.class.php, coresponding to this template

---
+++ Support/bin/symfony_navigator.rb
@@ -0,0 +1,40 @@
+class SymfonyNavigator
+  def initialize
+    @dir  = ENV['TM_DIRECTORY']
+    @file = ENV['TM_FILEPATH']
+    @line = ENV['TM_CURRENT_LINE']
+    @word = ENV['TM_CURRENT_WORD']
+    @proj = ENV['TM_PROJECT_DIRECTORY']
+  end
+
+  def guessAim
+    if /.*actions/.match @dir
+      if /.*function\ execute.*/.match @line
+        self.prepareGotoView
+      end
+    end
+    if /.*templates/.match @dir
+      self.prepareGotoAction
+    end
+  end
+
+  def prepareGotoView
+    viewName    = /.*function\ execute([^()]*).*/.match(@line)[1].strip.downcase
+    modulePath  = /(.*)\/actions/.match(@dir)[1]
+    @aim = "#{modulePath}/templates/#{viewName}Success.php"
+  end
+
+  def prepareGotoAction
+    modulePath  = /(.*)\/templates/.match(@dir)[1]
+    @aim = "#{modulePath}/actions/actions.class.php"
+  end
+
+  def goto
+    puts @aim
+    `open txmt://open/?url=file://#{@aim}` unless @aim == nil
+  end
+end
+
+sfNavigator = SymfonyNavigator.new
+sfNavigator.guessAim
+sfNavigator.goto
